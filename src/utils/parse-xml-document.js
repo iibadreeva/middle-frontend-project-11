867c5dc668e20eae08e02da10a923a82cad62d8c
Inna Badreeva <iibadreeva@mail.ruil>
2026-04-14 02:25:47 +0300

    eslint update

--- src/utils/parse-xml-document.js
+++ src/utils/parse-xml-document.js
@@ -1,1 +1,1 @@
-export const parseXmlDocument = content => new DOMParser().parseFromString(content, 'text/xml');
+export const parseXmlDocument = content => new DOMParser().parseFromString(content, 'text/xml')
